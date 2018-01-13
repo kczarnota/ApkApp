2e31f911e36b7b2d2677accf5c0e75d654b8337f
Konrad Czarnota <k.czarnota27@gmail.com>
2018-01-13 13:24:20 +0100

    First working map

--- app/src/main/java/com/example/konrad/indoorwayhackathon/IndoorwayApplication.java
+++ app/src/main/java/com/example/konrad/indoorwayhackathon/IndoorwayApplication.java
@@ -8,7 +8,7 @@ import com.indoorway.android.common.sdk.IndoorwaySdk;
 
 public class IndoorwayApplication extends Application
 {
-    public static final String API_KEY = "ba1817ea-6e91-482c8cf6-dbaa90b247cf";
+    public static final String API_KEY = "ba1817ea-6e91-482c-8cf6-dbaa90b247cf";
     public static final String ACTIVITY_TAG = IndoorwayApplication.class.getSimpleName();
 
     @Override
@@ -20,5 +20,6 @@ public class IndoorwayApplication extends Application
         // it's up to you when to initialize IndoorwaySdk, once initialized it will work forever!
         IndoorwaySdk.configure(API_KEY);
         Log.d(Utils.getTag(ACTIVITY_TAG), "onCreate: creating application");
+        Log.d(Utils.getTag(ACTIVITY_TAG), "onCreate: " + IndoorwaySdk.isConfigured());
     }
 }
--- app/src/main/java/com/example/konrad/indoorwayhackathon/Utils.java
+++ app/src/main/java/com/example/konrad/indoorwayhackathon/Utils.java
@@ -4,6 +4,10 @@ package com.example.konrad.indoorwayhackathon;
 public class Utils
 {
     public static final String APP_TAG = "IndoorwayHackathon";
+    public static final String BUILDING_UUID = "CScrSxCVhQg";
+    public static final String ZERO_FLOOR_UUID = "7-QLYjkafkE";
+    public static final String FIRST_FLOOR_UUID = "gVI7XXuBFCQ";
+    public static final String SECOND_FLOOR_UUID = "3-_M01M3r5w";
 
     public static String getTag(String classTag) {
         return APP_TAG + " " + classTag;
