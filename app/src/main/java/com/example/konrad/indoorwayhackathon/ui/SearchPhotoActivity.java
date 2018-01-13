e31b4955433b9ea36b7272c5738d7bd283769378
Konrad Czarnota <k.czarnota27@gmail.com>
2018-01-13 21:39:06 +0100

    Creating SearchPhotoActivity

---
+++ app/src/main/java/com/example/konrad/indoorwayhackathon/ui/SearchPhotoActivity.java
@@ -0,0 +1,19 @@
+package com.example.konrad.indoorwayhackathon.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.konrad.indoorwayhackathon.R;
+
+public class SearchPhotoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_photo);
+
+        //TODO get photo and it's location
+    }
+}
